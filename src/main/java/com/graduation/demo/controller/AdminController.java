30fc5c3770ba00c772ddf69f3b890fd44109b0de
userA-commits <1441367653@qq.com>
2022-03-11 23:40:22 +0800

    测试：对MybatisPlus生成代码进行简单测试

--- src/main/java/com/graduation/demo/controller/AdminController.java
+++ src/main/java/com/graduation/demo/controller/AdminController.java
@@ -1,9 +1,15 @@
 package com.graduation.demo.controller;
 
 
+import com.graduation.demo.entity.Admin;
+import com.graduation.demo.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
-
+    @Autowired
+    AdminService adminService;
+    @RequestMapping("/selectAll")
+    public String selectAll(){
+        List<Admin> adminList = adminService.list();
+        for(Admin a : adminList){
+            System.out.println(a);
+        }
+        return "index";
+    }
 }
 
