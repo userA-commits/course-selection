2601dc81aa31c4a1ac32e4e14704eac489515f51
userA-commits <1441367653@qq.com>
2022-03-14 03:10:24 +0800

    增加：添加了还未做完的shiro配置类 **有Bug

---
+++ src/main/java/com/graduation/demo/shiro/CustomRealm.java
@@ -0,0 +1,74 @@
+package com.graduation.demo.shiro;
+
+import com.graduation.demo.entity.Admin;
+import com.graduation.demo.entity.Role;
+import com.graduation.demo.service.AdminService;
+import com.graduation.demo.service.MenuService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class CustomRealm  extends AuthorizingRealm {
+
+    @Autowired
+    private AdminService adminService;
+    @Autowired
+    private MenuService menuService;
+
+    /**
+     * @MethodName doGetAuthorizationInfo
+     * @Description 权限配置类
+     * @Param [principalCollection]
+     * @Return AuthorizationInfo
+     * @Author WangShiLin
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        //获取登录用户名
+        String name = (String) principalCollection.getPrimaryPrincipal();
+        //查询用户名称
+        Admin admin = adminService.getById("");
+
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        //添加角色
+        simpleAuthorizationInfo.addRole("超级管理员");
+        //添加权限
+        for (Permissions permissions : role.getPermissions()) {
+            simpleAuthorizationInfo.addStringPermission(permissions.getPermissionsName());
+        }
+        return simpleAuthorizationInfo;
+    }
+
+    /**
+     * @MethodName doGetAuthenticationInfo
+     * @Description 认证配置类
+     * @Param [authenticationToken]
+     * @Return AuthenticationInfo
+     * @Author WangShiLin
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        if (StringUtils.isEmpty((CharSequence) authenticationToken.getPrincipal())) {
+            return null;
+        }
+        //获取用户信息
+        String name = authenticationToken.getPrincipal().toString();
+        Admin admin = adminService.getById("");
+        if (admin == null) {
+            //这里返回后会报出对应异常
+            return null;
+        } else {
+            //这里验证authenticationToken和simpleAuthenticationInfo的信息
+            SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(name, admin.getPassword().toString(), getName());
+            return simpleAuthenticationInfo;
+        }
+    }
+}
